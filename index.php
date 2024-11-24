778d472fd8fc0dbbb0b46c867afee64c3748d592
mvancaeyzeele <143445296+mvancaeyzeele@users.noreply.github.com>
2024-11-24 13:33:45 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/*******w******** 
+    
+    Name: Morgan VanCaeyzeele
+    Date: November 24th, 2024
+    Description: Final Project index page
+
+****************/
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Pelican Town Bulletin Board</title>
+</head>
+<body>
+    <h1>Pelican Town Bulletin Board</h1>
+    <a href="index.php">Home</a>
+    
+    <a href="post.php">New Posting</a>
+    
+    <p>Welcome to the online Pelican Town Bulletin Board system. Please visit the </p>
+</body>
+</html>
